6b5d86b8c522f93a628fe036c9c7ba8c3c1462ff
alirdurak <durakaliriza17@gmail.com>
2023-06-02 10:43:33 +0300

    tailwind style configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      "orange-light": "#FF6B00",
+      "background-black": "#0D0D0D",
+      "footer-bg": "#121212",
+      "title-gray": "#F0ECEC",
+      "content-gray": "#D0CCCC",
     },
   },
   plugins: [],
-}
+};
